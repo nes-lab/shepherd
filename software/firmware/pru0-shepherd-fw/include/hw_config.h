25415583eda94c63b5d7b19864af9e2a8cb7c3f2
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2021-01-12 20:48:22 +0100

    Update hw_config.h
    remove legacy code and stick to name "harvester" for recorder

--- software/firmware/pru0-shepherd-fw/include/hw_config.h
+++ software/firmware/pru0-shepherd-fw/include/hw_config.h
@@ -3,19 +3,12 @@
 
 #include "gpio.h"
 
-// TODO: remove when sampling.c is prepared for V2
-#define SPI_CS_ADC_PIN     	(P9_25)
-#define SPI_CS_ADC_MASK		BIT_SHIFT(SPI_CS_ADC_PIN)
-#define SPI_CS_DAC_PIN      	(P9_31)
-#define SPI_CS_DAC_MASK 	BIT_SHIFT(SPI_CS_DAC_PIN)
-
-
-#define SPI_CS_REC_DAC_PIN      (P9_27)
-#define SPI_CS_REC_DAC_MASK	BIT_SHIFT(SPI_CS_REC_DAC_PIN)
-#define SPI_CS_REC_V_ADC_PIN    (P9_41B)
-#define SPI_CS_REC_V_ADC_MASK 	BIT_SHIFT(SPI_CS_REC_V_ADC_PIN)
-#define SPI_CS_REC_C_ADC_PIN    (P9_25)
-#define SPI_CS_REC_C_ADC_MASK 	BIT_SHIFT(SPI_CS_REC_C_ADC_PIN)
+#define SPI_CS_HRV_DAC_PIN 	(P9_27)
+#define SPI_CS_HRV_DAC_MASK 	BIT_SHIFT(SPI_CS_HRV_DAC_PIN)
+#define SPI_CS_HRV_V_ADC_PIN 	(P9_41B)
+#define SPI_CS_HRV_V_ADC_MASK 	BIT_SHIFT(SPI_CS_HRV_V_ADC_PIN)
+#define SPI_CS_HRV_C_ADC_PIN 	(P9_25)
+#define SPI_CS_HRV_C_ADC_MASK 	BIT_SHIFT(SPI_CS_HRV_C_ADC_PIN)
 
 #define SPI_CS_EMU_DAC_PIN      (P9_28)
 #define SPI_CS_EMU_DAC_MASK 	BIT_SHIFT(SPI_CS_EMU_DAC_PIN)
@@ -30,8 +23,5 @@
 // both pins have a LED
 #define DEBUG_PIN0_MASK         BIT_SHIFT(P8_12)
 #define DEBUG_PIN1_MASK         BIT_SHIFT(P8_11)
-
-// TODO: has to stay as long as virtcap code is present (but will be replaced by virtual regulator)
-#define VIRTCAP_OUT_MASK     	(DEBUG_PIN0_MASK)
 
 #endif /* _HW_CONFIG_H_ */
