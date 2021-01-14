67378aaf28fae372b1adffa2ffc225fc80ebbc14
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2021-01-14 18:41:49 +0100

    Update calibration_default.py
    hw_rev2

--- software/python-package/shepherd/calibration_default.py
+++ software/python-package/shepherd/calibration_default.py
@@ -13,75 +13,63 @@ given the digital code in the DAC.
 :license: MIT, see LICENSE for more details.
 """
 
-# both current channels have 511mOhm shunt resistance
-R_SHT = 2.0
-# the instrumentation amplifiers are configured for gain of 50.25
-G_INST_AMP = 50.25
-# we use the ADC's internal reference with 4.096V
-V_REF_ADC = 4.096
-# range of current channels is +/- 0.625 * V_REF
-G_ADC_I = 0.625
-# range of voltage channels is 1.25 * V_REF
-G_ADC_V = 1.25
-# DACs has internal 2.5V reference
-V_REF_DAC = 2.5
-# gain of DAC-A (current) is set to 1
-G_DAC_I = 1
-# gain of DAC-B (voltage) is set to 2
-G_DAC_V = 2
-# bias voltage is generated from V_REF_DAC based on 1.2k and 100k resistors
-G_BIAS_AMP = 1.2 / 100
-
-# resistor values used in current source
-R_CS_REF_L = 1200
-R_CS_REF_H = 402
-R_CS_OUT = 30
+# both current channels have a 0.1 % shunt resistance of
+R_SHT = 1.00  # [ohm]
+# the instrumentation amplifiers are configured for gain of
+G_INST_AMP = 100 # [n]
+# we use the ADC's internal reference with
+V_REF_ADC = 4.096  # [V]
+# range of current channels is
+G_ADC_I = 1.25  # [gain / V_REF]
+# range of voltage channels is
+G_ADC_V = 1.25  # [gain / V_REF]
+# bit resolution of ADC
+M_ADC = 18  # [bit]
+# DACs use internal reference with
+V_REF_DAC = 2.5  # [V]
+# gain of DAC-CH-A is set to
+G_DAC_A = 2  # [n]
+# gain of DAC-CH-B is set to
+G_DAC_B = 2  # [n]
+# bit resolution of DAC
+M_DAC = 16  # [bit]
 
 
 def current_to_adc(current: float):
-    # voltage drop over shunt
-    v_sht = R_SHT * current
     # voltage on input of adc
-    v_adc = G_INST_AMP * v_sht
+    v_adc = G_INST_AMP * R_SHT * current
     # digital value according to ADC gain
-    return (2 ** 17) + v_adc / (G_ADC_I * V_REF_ADC) * (2 ** 17 - 1)
+    return v_adc * (2 ** M_ADC) / (G_ADC_I * V_REF_ADC)
 
 
 def adc_to_current(i_adc: float):
+    # voltage on input of adc
+    v_adc = i_adc * (G_ADC_I * V_REF_ADC) / (2 ** M_ADC)
     # current according to adc value
-    return (
-        (i_adc - (2 ** 17))
-        / (R_SHT * G_INST_AMP * (2 ** 17 - 1))
-        * (G_ADC_I * V_REF_ADC)
-    )
+    return v_adc / (R_SHT * G_INST_AMP)
 
 
 def voltage_to_adc(voltage: float):
     # digital value according to ADC gain
-    return voltage / (G_ADC_V * V_REF_ADC) * (2 ** 18 - 1)
+    return voltage * (2 ** M_ADC) / (G_ADC_V * V_REF_ADC)
 
 
 def adc_to_voltage(v_adc: float):
     # voltage according to ADC value
-    return v_adc * (G_ADC_V * V_REF_ADC) / (2 ** 18 - 1)
+    return v_adc * (G_ADC_V * V_REF_ADC) / (2 ** M_ADC)
 
 
-def dac_to_voltage(value: int):
-    return float(value) / (2 ** 16 - 1) * V_REF_DAC * G_DAC_V
+def dac_to_voltage_ch_a(value: int):
+    return float(value) * (V_REF_DAC * G_DAC_A) / (2 ** M_DAC)
 
 
-def voltage_to_dac(voltage: float):
-    return voltage / V_REF_DAC / G_DAC_V * (2 ** 16 - 1)
+def voltage_to_dac_ch_a(voltage: float):
+    return voltage * (2 ** M_DAC) / (V_REF_DAC * G_DAC_A)
 
 
-def dac_to_current(value: int):
-    # voltage at DAC output
-    v_dac = float(value) / (2 ** 16 - 1) * V_REF_DAC * G_DAC_I
-    # voltage after biasing with differential amplifier
-    v_dac_biased = v_dac - G_BIAS_AMP * V_REF_DAC
-    # current through ground referenced path of current source
-    i_gnd_ref = v_dac_biased / R_CS_REF_L
-    # voltage over reference resistor on high side of ground referenced path
-    v_gnd_ref_h = R_CS_REF_H * i_gnd_ref
-    # current through resistor on high side of output path
-    return v_gnd_ref_h / R_CS_OUT
+def dac_to_voltage_ch_b(value: int):
+    return float(value) * (V_REF_DAC * G_DAC_B) / (2 ** M_DAC)
+
+
+def voltage_to_dac_ch_b(voltage: float):
+    return voltage * (2 ** M_DAC) / (V_REF_DAC * G_DAC_B)
