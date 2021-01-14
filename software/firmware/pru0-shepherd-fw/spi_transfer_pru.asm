e8e2ece2c4c2da78dc7c9d4760d98cb235aef8fb
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2021-01-15 00:44:14 +0100

    Update spi_transfer_pru.asm
    adc fastread now performs in 630 ns

--- software/firmware/pru0-shepherd-fw/spi_transfer_pru.asm
+++ software/firmware/pru0-shepherd-fw/spi_transfer_pru.asm
@@ -7,57 +7,99 @@ MISO .set 2
    MOV r23, r23
 .endm
 
-    .global adc_readwrite ; code performs with 16.66 MHz, ~ 2060 ns CS low
+; ADS8691 - SPI-Mode-00
+; -> MSB begins with falling CS
+; -> begin with low CLK
+; -> Reads on rising CLK-Edge
+; -> transfer frame must contain 32 capture edges for writing (READING can be shorter)
+; (datasheet says: "shorter frame can result in erroneous device configuration")
+; (datasheet also: "host can use the short data transfer frame to read only the required number of MSB bits")
+
+    .global adc_readwrite ; code performs with 20 MHz, ~ 1760 ns CS low
 adc_readwrite:
     MOV r24, r14 ; Save input arg (CS pin)
-    LDI r20, 16 ; Load Counter for outloop
-    LDI r21, 18; Load Counter for inloop
-    CLR r30, r30, r24 ; Set CS low
+    LDI r20, 32 ; Load Counter for outloop
     LDI r14, 0 ; Clear return reg
+    CLR r30, r30, MOSI ; Set MOSI low
+    CLR r30, r30, r24 ; Set CS low
 
-adc_outloop_head:
+adc_io_loop_head:
     SUB r20, r20, 1 ; decrement shiftloop counter
-    QBBC adc_mosi_clr, r15, r20
-adc_mosi_set:
-    SET r30, r30, SCLK ; Set SCLK high
+    QBBC adc_io_mosi_clr, r15, r20
+adc_io_mosi_set:
+    CLR r30, r30, SCLK ; Set SCLK low
     SET r30, r30, MOSI ; Set MOSI high
-    JMP adc_outloop_tail
-adc_mosi_clr:
-    SET r30, r30, SCLK ; Set SCLK high
+    JMP adc_io_loop_mid
+adc_io_mosi_clr:
+    CLR r30, r30, SCLK ; Set SCLK low
     CLR r30, r30, MOSI ; Set MOSI low
     NOP
-adc_outloop_tail:
+adc_io_loop_mid:
     NOP
-    CLR r30, r30, SCLK ; Set SCLK low
-    QBLT adc_outloop_head, r20, 0
     NOP
-    CLR r30, r30, MOSI ; clear MOSI
-
-adc_inloop_head:
     SET r30, r30, SCLK ; Set SCLK high
-    SUB r21, r21, 1 ; decrement shiftloop counter
+    QBBC adc_io_miso_clr, r31, MISO
+adc_io_miso_set:
+    SET r14, r14, r20
+    QBLT adc_io_loop_head, r20, 0
+    JMP adc_io_end
+adc_io_miso_clr:
     NOP
-    NOP
-    CLR r30, r30, SCLK ; Set SCLK low
-    QBBC adc_miso_clr, r31, MISO
-adc_miso_set:
-    SET r14, r14, r21
-    QBLT adc_inloop_head, r21, 0
-    JMP adc_end
-adc_miso_clr:
-    NOP
-    QBLT adc_inloop_head, r21, 0
+    QBLT adc_io_loop_head, r20, 0
 
-adc_end:
+adc_io_end:
     SET r30, r30, r24 ; set CS high
+    CLR r30, r30, MOSI ; Set MOSI low
     JMP r3.w2
 
+
+
+    .global adc_fastread ; code performs with 28-33 MHz (18 bit read), ~ 550-630 ns CS low
+adc_fastread:
+    MOV r24, r14 ; Save input arg (CS pin)
+    LDI r20, 18 ; Load Counter for loop
+    LDI r14, 0 ; Clear return reg
+    CLR r30, r30, MOSI ; Set MOSI low
+    CLR r30, r30, r24 ; Set CS low
+
+adc_readloop_head: ; 6 - 7 ticks, depending on input
+    CLR r30, r30, SCLK ; Set SCLK low
+    SUB r20, r20, 1 ; decrement shiftloop counter
+    NOP
+    SET r30, r30, SCLK ; Set SCLK High
+    QBBC adc_readloop_tail, r31, MISO
+adc_miso_set:
+    SET r14, r14, r20
+adc_readloop_tail:
+    QBLT adc_readloop_head, r20, 0
+    JMP adc_read_end
+
+adc_nop_prepare: ; not active atm, 50 MHz (14 bit nop)
+    LDI r20, 14 ; Load Counter for fast loop
+adc_nop_loop:
+    CLR r30, r30, SCLK ; Set SCLK low
+    SUB r20, r20, 1 ; decrement shiftloop counter
+    SET r30, r30, SCLK ; Set SCLK High
+    QBLT adc_nop_loop, r20, 0
+
+adc_read_end:
+    SET r30, r30, r24 ; set CS high
+;    CLR r30, r30, SCLK ; Set SCLK low
+    JMP r3.w2
+
+
+
+; DAC8562
+; -> MSB begins with falling CS
+; -> begin with high CLK
+; -> Reads on falling CLK-Edge
+; -> transfer frame must contain 24 capture edges for writing
 
     .global dac_write  ; code performs with 25 MHz, ~ 980 ns CS low
 dac_write:
     LDI r20, 24 ; Load Counter for outloop
+    SET r30, r30, SCLK ; Set SCLK high
     CLR r30, r30, r14 ; Set CS low
-    NOP
 
 dac_loop_head:
     SUB r20, r20, 1 ; Decrement counter
