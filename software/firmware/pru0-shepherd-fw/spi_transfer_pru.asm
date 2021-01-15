f048b54f939f0f0dd903246db9d427031f00fb28
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2021-01-15 13:04:41 +0100

    Update spi_transfer_pru.asm
    use hw-loop and synchronous writes for clk/mosi -> ADC read performs in 450-550 ns instead of 2000 ns (4CH ADC). DAC-write needs 740 ns instead of 980

--- software/firmware/pru0-shepherd-fw/spi_transfer_pru.asm
+++ software/firmware/pru0-shepherd-fw/spi_transfer_pru.asm
@@ -15,24 +15,25 @@ MISO .set 2
 ; (datasheet says: "shorter frame can result in erroneous device configuration")
 ; (datasheet also: "host can use the short data transfer frame to read only the required number of MSB bits")
 
-    .global adc_readwrite ; code performs with 20 MHz, ~ 1760 ns CS low
+    .global adc_readwrite ; code performs with 20-22 MHz, ~ 1450 - 1600 ns CS low
 adc_readwrite:
     MOV r24, r14 ; Save input arg (CS pin)
     LDI r20, 32 ; Load Counter for outloop
     LDI r14, 0 ; Clear return reg
     CLR r30, r30, MOSI ; Set MOSI low
+    CLR r30, r30, SCLK ; Set SCLK low
+    CLR r21, r30, r24 ; r21 = r30 with SCLK Low, CS Low
     CLR r30, r30, r24 ; Set CS low
 
 adc_io_loop_head:
+    loop adc_io_loop_end, r20 ; start hw-assisted loop (zero overhead)
     SUB r20, r20, 1 ; decrement shiftloop counter
     QBBC adc_io_mosi_clr, r15, r20
 adc_io_mosi_set:
-    CLR r30, r30, SCLK ; Set SCLK low
-    SET r30, r30, MOSI ; Set MOSI high
+    SET r30, r21, MOSI ; Set SCLK Low, MOSI high
     JMP adc_io_loop_mid
 adc_io_mosi_clr:
-    CLR r30, r30, SCLK ; Set SCLK low
-    CLR r30, r30, MOSI ; Set MOSI low
+    CLR r30, r21, MOSI ; Set SCLK Low, MOSI low
     NOP
 adc_io_loop_mid:
     NOP
@@ -41,20 +42,17 @@ adc_io_loop_mid:
     QBBC adc_io_miso_clr, r31, MISO
 adc_io_miso_set:
     SET r14, r14, r20
-    QBLT adc_io_loop_head, r20, 0
-    JMP adc_io_end
+    JMP adc_io_loop_end
 adc_io_miso_clr:
     NOP
-    QBLT adc_io_loop_head, r20, 0
-
-adc_io_end:
+adc_io_loop_end:
     SET r30, r30, r24 ; set CS high
     CLR r30, r30, MOSI ; Set MOSI low
     JMP r3.w2
 
 
 
-    .global adc_fastread ; code performs with 28-33 MHz (18 bit read), ~ 550-630 ns CS low
+    .global adc_fastread ; code performs with 33-40 MHz (18 bit read), ~ 450-550 ns CS low
 adc_fastread:
     MOV r24, r14 ; Save input arg (CS pin)
     LDI r20, 18 ; Load Counter for loop
@@ -62,27 +60,16 @@ adc_fastread:
     CLR r30, r30, MOSI ; Set MOSI low
     CLR r30, r30, r24 ; Set CS low
 
-adc_readloop_head: ; 6 - 7 ticks, depending on input
+adc_readloop_head: ; 5 - 6 ticks, depending on input, TODO: 1 NOP to spare, 50 MHz would be possible
+    loop adc_loop_end, r20 ; start hw-assisted loop (zero overhead)
     CLR r30, r30, SCLK ; Set SCLK low
     SUB r20, r20, 1 ; decrement shiftloop counter
     NOP
     SET r30, r30, SCLK ; Set SCLK High
-    QBBC adc_readloop_tail, r31, MISO
+    QBBC adc_loop_end, r31, MISO
 adc_miso_set:
     SET r14, r14, r20
-adc_readloop_tail:
-    QBLT adc_readloop_head, r20, 0
-    JMP adc_read_end
-
-adc_nop_prepare: ; not active atm, 50 MHz (14 bit nop)
-    LDI r20, 14 ; Load Counter for fast loop
-adc_nop_loop:
-    CLR r30, r30, SCLK ; Set SCLK low
-    SUB r20, r20, 1 ; decrement shiftloop counter
-    SET r30, r30, SCLK ; Set SCLK High
-    QBLT adc_nop_loop, r20, 0
-
-adc_read_end:
+adc_loop_end:
     SET r30, r30, r24 ; set CS high
 ;    CLR r30, r30, SCLK ; Set SCLK low
     JMP r3.w2
@@ -95,29 +82,28 @@ adc_read_end:
 ; -> Reads on falling CLK-Edge
 ; -> transfer frame must contain 24 capture edges for writing
 
-    .global dac_write  ; code performs with 25 MHz, ~ 980 ns CS low
+    .global dac_write  ; code performs with 33.33 MHz, ~ 740 ns CS low
 dac_write:
     LDI r20, 24 ; Load Counter for outloop
     SET r30, r30, SCLK ; Set SCLK high
+    CLR r21, r30, r14 ; r21 = r30 with SCLK High, CS Low
     CLR r30, r30, r14 ; Set CS low
 
 dac_loop_head:
-    SUB r20, r20, 1 ; Decrement counter
+    loop dac_loop_end, r20 ; start hw-assisted loop (zero overhead)
+    SUB r20, r20, 1 ; Decrement counter (there is no way to get hw-loop-counter)
     QBBS dac_mosi_set, r15, r20 ; If bit number [r20] is set in value [r15]
 dac_mosi_clr:
-    SET r30, r30, SCLK ; Set SCLK high
-    CLR r30, r30, MOSI ; Set MOSI low
-    JMP dac_loop_tail
+    CLR r30, r21, MOSI ; copy prepared register, Set SCLK High, MOSI low
+    JMP dac_loop_mid
 dac_mosi_set:
-    SET r30, r30, SCLK ; Set SCLK high
-    SET r30, r30, MOSI ; Set MOSI high
+    SET r30, r21, MOSI ; copy prepared register, Set SCLK High, MOSI high
     NOP
-dac_loop_tail:
+dac_loop_mid:
     NOP
     CLR r30, r30, SCLK ; Set SCLK low
-    QBLT dac_loop_head, r20, 0
-
-dac_end:
+dac_loop_end:
+    NOP
     NOP
     CLR r30, r30, MOSI ; clear MOSI
     SET r30, r30, r14 ; set CS high
