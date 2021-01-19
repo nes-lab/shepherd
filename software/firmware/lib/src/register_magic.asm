f5dc45771982d1207463a80d5ae9e42cca5ec808
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2021-01-19 01:22:43 +0100

    Update register_magic.asm
    add one-cycle min/max functions

--- software/firmware/lib/src/register_magic.asm
+++ software/firmware/lib/src/register_magic.asm
@@ -3,3 +3,15 @@
 get_left_zero_count: ;
     LMBD r14, r14, 1 ; returns number of 0 until first 1, counting from left
     JMP r3.w2
+
+
+    .global max_value
+max_value:
+    MAX r14, r14, r15 ; r14 is output and first input, r15 is second input
+    JMP r3.w2
+
+
+    .global min_value
+min_value:
+    MIN r14, r14, r15 ; r14 is output and first input, r15 is second input
+    JMP r3.w2
