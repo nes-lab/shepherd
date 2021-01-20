855a88d271572eddf81c6908169d9c6f4c0e5626
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2021-01-20 22:53:41 +0100

    Update AM335x_PRU.cmd
    Extend PRU0 IMEM from 8 kb to 12 kb

--- software/firmware/lib/AM335x_PRU.cmd
+++ software/firmware/lib/AM335x_PRU.cmd
@@ -13,7 +13,7 @@
 MEMORY
 {
       PAGE 0:
-	PRU_IMEM		: org = 0x00000000 len = 0x00002000  /* 8 kB PRU0 Instruction RAM */
+	PRU_IMEM		: org = 0x00000000 len = 0x00003000  /* 12 kB PRU0 Instruction RAM, TODO: len was 0x2000, unused 0x1fe4, -> .text did not fit anymore */
 
       PAGE 1:
 
