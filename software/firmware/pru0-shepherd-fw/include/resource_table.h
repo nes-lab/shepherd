cb17b50a75f55f83332bfbd4e9dd2b06e751eef5
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2021-02-22 21:32:44 +0100

    declutter resource table

--- software/firmware/pru0-shepherd-fw/include/resource_table.h
+++ software/firmware/pru0-shepherd-fw/include/resource_table.h
@@ -6,15 +6,9 @@
 struct my_resource_table {
 	struct resource_table base;
 
-	uint32_t offset[3]; /* Should match 'num' in actual definition */
+	uint32_t offset[1]; /* Should match 'num' in actual definition */
 
-	/* rpmsg vdev entry */
-	struct fw_rsc_vdev rpmsg_vdev;
-	struct fw_rsc_vdev_vring rpmsg_vring0;
-	struct fw_rsc_vdev_vring rpmsg_vring1;
-
-	/* intc definition */
-	struct fw_rsc_custom pru_ints;
+	/* resource definition */
 	struct fw_rsc_carveout shared_mem;
 };
 
--- software/firmware/pru1-shepherd-fw/include/resource_table.h
+++ software/firmware/pru1-shepherd-fw/include/resource_table.h
@@ -36,23 +36,7 @@
 
 #include <stddef.h>
 #include <rsc_types.h>
-#include "pru_virtio_ids.h"
-
 #include "commons.h"
-/*
- * Sizes of the virtqueues (expressed in number of buffers supported,
- * and must be power of 2)
- */
-#define PRU_RPMSG_VQ0_SIZE  16U
-#define PRU_RPMSG_VQ1_SIZE  16U
-
-/*
- * The feature bitmap for virtio rpmsg
- */
-#define VIRTIO_RPMSG_F_NS   0U //name service notifications
-
-/* This firmware supports name service notifications as one of its features */
-#define RPMSG_PRU_C0_FEATURES (1U << VIRTIO_RPMSG_F_NS)
 
 /* Definition for unused interrupts */
 #define HOST_UNUSED         255U
@@ -61,18 +45,12 @@
 /* List of system events in TRM Table 4.22 */
 struct ch_map pru_intc_map[] = {
 	{ HOST_PRU_EVT_TIMESTAMP, 0 }, // Ext int for sync from ARM host
-	{ 18, 2 }, // Interrupt for RPMSG from PRU to ARM host
 };
 
 struct my_resource_table {
 	struct resource_table base;
 
-	uint32_t offset[2]; /* Should match 'num' in actual definition */
-
-	/* rpmsg vdev entry */
-	struct fw_rsc_vdev rpmsg_vdev;
-	struct fw_rsc_vdev_vring rpmsg_vring0;
-	struct fw_rsc_vdev_vring rpmsg_vring1;
+	uint32_t offset[1]; /* Should match 'num' in actual definition */
 
 	/* intc definition */
 	struct fw_rsc_custom pru_ints;
@@ -83,42 +61,12 @@ struct my_resource_table {
 struct my_resource_table resourceTable = {
         {
                 1, /* Resource table version: only version 1 is supported by the current driver */
-                2, /* number of entries in the table */
+                1, /* number of entries in the table */
                 { 0U, 0U}, /* reserved, must be zero */
         },
         /* offsets to entries */
         {
-                offsetof(struct my_resource_table, rpmsg_vdev),
                 offsetof(struct my_resource_table, pru_ints),
-        },
-
-        /* rpmsg vdev entry */
-        {
-                (uint32_t)TYPE_VDEV, //type
-                (uint32_t)VIRTIO_ID_RPMSG, //id
-                (uint32_t)0, //notifyid
-                (uint32_t)RPMSG_PRU_C0_FEATURES, //dfeatures
-                (uint32_t)0, //gfeatures
-                (uint32_t)0, //config_len
-                (uint8_t)0, //status
-                (uint8_t)2, //num_of_vrings, only two is supported
-                { (uint8_t)0, (uint8_t)0 }, //reserved
-                /* no config data */
-        },
-        /* the two vrings */
-        {
-                0, //da, will be populated by host, can't pass it in
-                16, //align (bytes),
-                PRU_RPMSG_VQ0_SIZE, //num of descriptors
-                0, //notifyid, will be populated, can't pass right now
-                0 //reserved
-        },
-        {
-                0, //da, will be populated by host, can't pass it in
-                16, //align (bytes),
-                PRU_RPMSG_VQ1_SIZE, //num of descriptors
-                0, //notifyid, will be populated, can't pass right now
-                0 //reserved
         },
 
         {
@@ -133,7 +81,7 @@ struct my_resource_table resourceTable = {
                            maps to host interrupt 0 */
                         0,
                         HOST_UNUSED,
-                        2,
+			HOST_UNUSED,
                         HOST_UNUSED,
                         HOST_UNUSED,
                         HOST_UNUSED,
