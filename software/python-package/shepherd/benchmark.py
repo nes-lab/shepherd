c17360f84c3913dca63d3087326375e548842f24
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2021-09-23 17:29:04 +0200

    Create benchmark.py

---
+++ software/python-package/shepherd/benchmark.py
@@ -0,0 +1,20 @@
+import logging
+import click
+from shepherd.cli import consoleHandler
+
+logger = logging.getLogger("shepherd")
+logger.addHandler(consoleHandler)
+
+def generate_rec_data():
+    print("None")
+
+@click.command()
+@click.option("--duration", "-d", type=float, default=180, help="Duration of recording in seconds")
+def cli(duration):
+    logging._srcfile = None
+    logging.logThreads = 0
+    logging.logProcesses = 0
+
+
+if __name__ == "__main__":
+    cli()
