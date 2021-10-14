375ed7fbbf210f6e4d357159419c882ba32c5dec
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2021-10-14 13:42:13 +0200

    Update testbench_bug.py

--- software/python-package/shepherd/testbench_bug.py
+++ software/python-package/shepherd/testbench_bug.py
@@ -6,46 +6,9 @@ import h5py
 from typing import NoReturn
 
 
-def record(output_path: Path, duration: int):
-    length = 10_000
-    with LogWriter(output_path) as drain:
-        for _ in range(duration * 10):
-            buf = {"voltage": np.zeros(length, dtype="u4"),
-                   "current": np.zeros(length, dtype="u4"),
-                   "timestamp": int(time.time() * 10 ** 9),}
-            drain.write_buffer(buf)
-
-
-def emulate(input_path: Path, output_path: Path):
-    with LogReader(input_path) as source, LogWriter(output_path) as drain:
-        for data_buf in source.read_buffers():
-            buf = {"voltage": data_buf["voltage"],
-                   "current": data_buf["current"],  # np.zeros(length, dtype="u4"),
-                   "timestamp": int(time.time() * 10 ** 9),}
-            drain.write_buffer(buf)
-
-
-if __name__ == "__main__":
-    duration = 25 * 60
-    benchmark_path = Path("/var/shepherd/recordings")
-    file_rec = benchmark_path / "db_voltage04h.h5" #"benchmark_rec.h5"
-    file_emu = benchmark_path / "benchmark_emu.h5"
-
-    print(h5py.version.info)
-    print("ram is leaking during processing, not during generation -> only difference: h5py lzf-reading")
-
-    print("Starting Generating")
-    #record(output_path=file_rec, duration=duration)
-    print(f"cleaned {gc.collect()} obj")
-
-    print("Starting Processing")
-    emulate(input_path=file_rec, output_path=file_emu)
-    print(f"cleaned {gc.collect()} obj")
-
-
 class LogWriter(object):
 
-    compression_algo = "lzf"
+    compression_algo = None # "lzf"
 
     def __init__(
             self,
@@ -77,7 +40,7 @@ class LogWriter(object):
             (self.data_inc,),
             dtype="u8",
             maxshape=(None,),
-            chunks=self.chunk_shape,
+            chunks=True, # self.chunk_shape,
             compression=LogWriter.compression_algo,
         )
         self.data_grp.create_dataset(
@@ -85,7 +48,7 @@ class LogWriter(object):
             (self.data_inc,),
             dtype="u4",
             maxshape=(None,),
-            chunks=self.chunk_shape,
+            chunks=True, #self.chunk_shape,
             compression=self.compression_algo,
         )
         self.data_grp.create_dataset(
@@ -93,7 +56,7 @@ class LogWriter(object):
             (self.data_inc,),
             dtype="u4",
             maxshape=(None,),
-            chunks=self.chunk_shape,
+            chunks=True, #self.chunk_shape,
             compression=LogWriter.compression_algo,
         )
 
@@ -160,3 +123,77 @@ class LogReader(object):
             db = {"voltage": self.ds_voltage[idx_start:idx_end],
                   "current": self.ds_current[idx_start:idx_end]}
             yield db
+
+
+def record(output_path: Path, duration: int):
+    length = 10_000
+    with LogWriter(output_path) as drain:
+        for _ in range(duration * 10):
+            buf = {"voltage": np.zeros(length, dtype="u4"),
+                   "current": np.zeros(length, dtype="u4"),
+                   "timestamp": int(time.time() * 10 ** 9),}
+            drain.write_buffer(buf)
+
+
+def emulate(input_path: Path, output_path: Path):
+    with LogReader(input_path) as source, LogWriter(output_path) as drain:
+        for data_buf in source.read_buffers():
+            buf = {"voltage": data_buf["voltage"],
+                   "current": data_buf["current"],  # np.zeros(length, dtype="u4"),
+                   "timestamp": int(time.time() * 10 ** 9),}
+            drain.write_buffer(buf)
+
+
+if __name__ == "__main__":
+    duration = 200 * 60
+    #benchmark_path = Path("/var/shepherd/recordings")
+    benchmark_path = Path(".")
+    file_rec = benchmark_path / "benchmark_rec.h5"
+    file_emu = benchmark_path / "benchmark_emu.h5"
+
+    print(h5py.version.info)
+    print("ram is leaking during processing, not during generation -> only difference: h5py lzf-reading")
+
+    if not file_rec.exists():
+        print("Starting Generating (hdf5 write)")
+        record(output_path=file_rec, duration=duration)
+        print(f"[GC] cleaned {gc.collect()} obj")
+
+    print("Starting Processing (hdf5 read & write)")
+    emulate(input_path=file_rec, output_path=file_emu)
+    print(f"[GC] cleaned {gc.collect()} obj")
+    
+'''
+Bug / script: 
+- first file-creation with record() works fine, 200 min simulation with stable ~ 33 mb RAM usage 
+- Ram is leaking during reading a previously created file in emulate()
+    - up to ~ 180 mb ram for simulated 200 min duration
+- tracemalloc and molly showed no leaking in the python-code itself
+
+no influence:
+- vlen-elements
+- custom chunk-size
+- compression
+
+tested systems: 
+Beaglebone, arm7
+    - python 3.6, h5py 2.7.1, numpy 1.13.3
+    - python 3.9.5, h5py 2.10.0, numpy 1.19.5
+    - python 3.9.5, h5py 3.4.0, numpy 1.21.2
+Lenovo W530, Intel x64 CPU
+    - python 3.9.7, h5py 3.4.0, numpy 1.21.2
+AMD Ryzen 5700
+    - python 3.10, h5py 3.4.0, 
+        
+Output of h5py.version.info
+h5py    3.4.0
+HDF5    1.12.1
+Python  3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)]
+sys.platform    win32
+sys.maxsize     9223372036854775807
+numpy   1.21.2
+cython (built with) 0.29.24
+numpy (built against) 1.19.3
+HDF5 (built against) 1.12.1
+    
+'''
