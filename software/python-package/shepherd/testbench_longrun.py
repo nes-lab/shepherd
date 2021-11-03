ddc6995112935079bbae2eefcbece99268eda3fb
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2021-11-03 17:53:39 +0100

    Update testbench_longrun.py

--- software/python-package/shepherd/testbench_longrun.py
+++ software/python-package/shepherd/testbench_longrun.py
@@ -6,10 +6,9 @@ from shepherd import record, emulate
 # sudo python3 /opt/shepherd/software/python-package/shepherd/testbench_longrun.py
 
 if __name__ == "__main__":
-    duration = 100 * 60
+    duration = 600 * 60
     benchmark_path = Path("/var/shepherd/recordings")
     file_rec = benchmark_path / "benchmark_rec.h5"
-    #file_rec = benchmark_path / "db_voltage04h.h5"
     file_emu1 = benchmark_path / "benchmark_emu1.h5"
     file_emu2 = benchmark_path / "benchmark_emu2.h5"
 
@@ -17,7 +16,8 @@ if __name__ == "__main__":
         print("Starting Harvesting")
         record(output_path=file_rec,
                duration=duration,
-               force_overwrite=True,)
+               force_overwrite=True,
+               use_cal_default=True,)
 
     print("Starting Emulation1, only logging of SysUtil-Stats")
     emulate(input_path=file_rec,
@@ -27,7 +27,8 @@ if __name__ == "__main__":
             settings_virtsource="BQ25570s",
             skip_log_gpio=True,
             skip_log_current=True,
-            skip_log_voltage=True,)
+            skip_log_voltage=True,
+            )
 
     print("Starting Emulation2, ")
     emulate(input_path=file_rec,
@@ -35,5 +36,3 @@ if __name__ == "__main__":
             duration=duration,
             force_overwrite=True,
             settings_virtsource="BQ25570s",)
-
-
