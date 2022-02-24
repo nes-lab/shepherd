00b1f02c3afc3129f76bcddcfe4c54ec894dd97f
Kai Geissdoerfer <kai.geissdoerfer@tu-dresden.de>
2022-02-24 10:18:47 +0100

    firmware: comments for nRF52 device programming layer

--- software/firmware/pru0-shepherd-fw/programmer/device_nrf52.c
+++ software/firmware/pru0-shepherd-fw/programmer/device_nrf52.c
@@ -7,7 +7,14 @@
 #include "programmer/nrf52840.h"
 #include "programmer/nrf52840_bitfields.h"
 
-int mem_write(uint32_t addr, uint32_t data)
+/**
+ * Writes a word to the specified address in memory. Note that this routine can NOT
+ * be used to write to non-volatile flash memory.
+ *
+ * @param addr target memory address
+ * @param data word to be written
+ */
+static int mem_write(uint32_t addr, uint32_t data)
 {
 	int rc;
 
@@ -16,24 +23,34 @@ int mem_write(uint32_t addr, uint32_t data)
 	if ((rc = ap_write(AP_REG_DRW, data)))
 		return rc;
 
+	/* dummy read to make sure previous transfer has finished */
 	dp_read(&data, DP_REG_RDBUFF);
 	return 0;
 }
 
-int mem_read(uint32_t *data, uint32_t addr)
+/**
+ * Reads a word from the specified address in memory. Can be used for both, volatile memory and
+ * non-volatile flash memory.
+ *
+ * @param dst pointer to destination
+ * @param addr target memory address
+ */
+static int mem_read(uint32_t *dst, uint32_t addr)
 {
 	int rc;
 
 	if ((rc = ap_write(AP_REG_TAR, addr)))
 		return rc;
 
-	if ((rc = ap_read(data, AP_REG_DRW)))
+	if ((rc = ap_read(dst, AP_REG_DRW)))
 		return rc;
 
-	return ap_read(data, AP_REG_DRW);
+	/* dummy read to make sure previous transfer has finished */
+	return ap_read(dst, AP_REG_DRW);
 }
 
-int dev_halt()
+/* Halts the core */
+static int dev_halt()
 {
 	int rc = mem_write(CoreDebug_BASE + offsetof(CoreDebug_Type, DHCSR),
 			   (0xA05Fu << CoreDebug_DHCSR_DBGKEY_Pos) | CoreDebug_DHCSR_C_HALT_Msk | CoreDebug_DHCSR_C_DEBUGEN_Msk);
@@ -41,7 +58,8 @@ int dev_halt()
 	return rc;
 }
 
-int dev_continue()
+/* Continues execution */
+static int dev_continue()
 {
 	int rc;
 	if ((rc = mem_write(CoreDebug_BASE + offsetof(CoreDebug_Type, DEMCR), 0x0)))
@@ -50,7 +68,8 @@ int dev_continue()
 	return mem_write(CoreDebug_BASE + offsetof(CoreDebug_Type, DHCSR), (0xA05Fu << CoreDebug_DHCSR_DBGKEY_Pos) | CoreDebug_DHCSR_C_DEBUGEN_Msk);
 }
 
-int dev_reset_halt()
+/* Halts and resets the core */
+static int dev_reset_halt()
 {
 	int rc;
 
@@ -81,6 +100,7 @@ int dev_reset_halt()
 	return -1;
 }
 
+/* Waits for non-volatile memory controller to be ready for a new write */
 static int nvm_wait(unsigned int retries)
 {
 	int rc;
@@ -94,6 +114,7 @@ static int nvm_wait(unsigned int retries)
 	return 0;
 }
 
+/* Disables write-protection of non-volatile flash memory */
 static int nvm_wp_disable(void)
 {
 	int rc;
@@ -104,11 +125,13 @@ static int nvm_wp_disable(void)
 	return nvm_wait(64);
 }
 
+/* Enables write-protection of non-volatile flash memory */
 static int nvm_wp_enable(void)
 {
 	return mem_write(NRF_NVMC_BASE + offsetof(NRF_NVMC_Type, CONFIG), 0x0);
 }
 
+/* Erases the whole flash */
 static int nvm_erase(void)
 {
 	int rc;
@@ -118,18 +141,36 @@ static int nvm_erase(void)
 	if ((rc = mem_write(NRF_NVMC_BASE + offsetof(NRF_NVMC_Type, ERASEALL), 0x1)))
 		return rc;
 
+	/* Wait until the nvm controller has finished the operation */
 	return nvm_wait(1024);
 }
 
-static int nvm_write(uint32_t addr, uint32_t data)
+/**
+ * Writes a word to the non-volatile flash memory
+ *
+ * @param target memory address
+ * @param data word to be written
+ *
+ */
+static int nvm_write(uint32_t dst, uint32_t data)
 {
 	int rc;
 	if ((rc = nvm_wait(64)))
 		return rc;
 
-	return mem_write(addr, data);
+	return mem_write(dst, data);
 }
 
+/**
+ * Prepares the nRF52 for access. After execution, the core should be reset, halted
+ * and ready to receive writes to the non-volatile flash memory.
+ *
+ * @param pin_swdclk pin number for SWDCLK signal. Note: Only supports pins of GPIO port 0.
+ * @param pin_swdio pin number for SWDIO signal. Note: Only supports pins of GPIO port 0.
+ * @param f_clk frequency of SWDCLK signal
+ *
+ * @returns DRV_ERR_OK on success
+ */
 static int open(unsigned int pin_swdclk, unsigned int pin_swdio, unsigned int f_clk)
 {
 	uint32_t data;
@@ -151,6 +192,7 @@ static int open(unsigned int pin_swdclk, unsigned int pin_swdio, unsigned int f_
 	return DRV_ERR_OK;
 }
 
+/* Disables access to and communication with the nRF52. After this, the core should be reset and running */
 static int close(void)
 {
 	nvm_wp_enable();
@@ -160,4 +202,11 @@ static int close(void)
 	return DRV_ERR_OK;
 }
 
-device_driver_t nrf52_driver = { .open = open, .erase = nvm_erase, .write = nvm_write, .close = close, .read = mem_read, .word_width = 32 };
+device_driver_t nrf52_driver = {
+	.open = open,
+	.erase = nvm_erase,
+	.write = nvm_write,
+	.close = close,
+	.read = mem_read,
+	.word_width_bytes = 4,
+};
