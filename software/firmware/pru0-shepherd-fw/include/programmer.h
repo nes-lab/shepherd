727825016d51f6346cdf614b5a3ba36ae813d667
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2022-04-05 14:48:22 +0200

    remove programmer-template from pru0

--- software/firmware/pru0-shepherd-fw/include/programmer.h
+++
@@ -1,9 +0,0 @@
-#ifndef PRU_FIRMWARE_PRU0_SHEPHERD_FW_INCLUDE_PROGRAMMER_H
-#define PRU_FIRMWARE_PRU0_SHEPHERD_FW_INCLUDE_PROGRAMMER_H
-
-#include "commons.h"
-
-void programmer(volatile struct SharedMem * shared_mem,
-	    volatile struct SampleBuffer * buffers_far);
-
-#endif //PRU_FIRMWARE_PRU0_SHEPHERD_FW_INCLUDE_PROGRAMMER_H
