d065854239b082ac111c6dabaa3e314f0879fa5b
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2022-08-01 02:29:39 +0200

    prepare c-code to be imported in cython

--- software/firmware/lib/include/simple_lock.h
+++ software/firmware/lib/include/simple_lock.h
@@ -9,8 +9,8 @@ typedef struct {
     bool_ft lock_pru1;
 }__attribute__((packed)) simple_mutex_t;
 
-inline void simple_mutex_enter(volatile simple_mutex_t * mutex);
-inline void simple_mutex_exit(volatile simple_mutex_t * mutex);
+inline void simple_mutex_enter(volatile simple_mutex_t *const mutex);
+inline void simple_mutex_exit(volatile simple_mutex_t *const mutex);
 
 
 #endif /* __SIMPLE_LOCK_H_ */
