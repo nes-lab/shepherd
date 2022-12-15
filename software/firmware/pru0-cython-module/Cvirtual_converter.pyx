8abb7273056d0ec79dc4cf30a34be5064e28fa18
vishal1995sivakumar <14985440+WiresharkIO@users.noreply.github.com>
2022-12-15 19:16:18 +0100

    updated testing.py and cvirtual_converter.pyx towards compilation

--- software/firmware/pru0-cython-module/Cvirtual_converter.pyx
+++ software/firmware/pru0-cython-module/Cvirtual_converter.pyx
@@ -1,10 +1,6 @@
 #!python
 #cython: language_level=3
 
-#cimport hvirtual_converter
-"""vishal.shepherd.software.firmware.pru0_shepherd_fw."""
-#from hvirtual_converter cimport (converter_calc_inp_power, converter_calc_out_power, converter_update_cap_storage, set_P_input_fW, set_P_output_fW, set_V_intermediate_uV, get_P_input_fW, get_P_output_fW, get_V_intermediate_uV, get_V_intermediate_raw, get_I_mid_out_nA)
-
 cimport hvirtual_converter
 from libc.stdint cimport *
 from libc.stdint cimport uint32_t
@@ -13,49 +9,107 @@ from libc.stdlib cimport free
 from libc.stdlib cimport malloc
 
 import pyximport
-
-#cdef extern from "virtual_converter.c":
-#	cdef static uint32_t get_input_efficiency_n8(uint32_t voltage_uV, uint32_t current_nA)
-#	cdef static uint32_t get_output_inv_efficiency_n4(uint32_t current_nA)
+import math
 
 """
-	The language used here is a special mix of C and Python.
-	However it will look fairly familiar to Python developers.
-
-
-	This section looks like a regular Python function — because it just creates a Python function that has access to the C functions.
-	These are Python-Wrappers...
+The language used here is a special mix of C and Python. However it will look fairly familiar to Python developers.
 """
 
+# static function handling
+#cdef uint32_t get_input_efficiency_n8(const uint32_t voltage_uV, const uint32_t current_nA)
+
+# structure declaration
+cdef const hvirtual_converter.ConverterConfig *cfg
+#ctypedef hvirtual_converter.ConverterConfig *const config
+
 """
-def converter_initialize(*config):
-	hvirtual_converter.converter_initialize(*config)
+This section looks like a regular Python function — because it just creates a Python function that has access to the C functions. These are Python-Wrappers...
 """
 
-def converter_calc_inp_power(input_voltage_uV, input_current_nA):
-	return hvirtual_converter.converter_calc_inp_power(input_voltage_uV, input_current_nA)
+cdef class VirtualConverter:
+	# TODO: each FN now needs data-conversion from python-objects to c-objects and reverse for return-values
+	
+	def __init__(self, vs_config: list):
+		cdef hvirtual_converter.ConverterConfig* config
+		self.config.converter_mode 			 						= vs_config[0]
+		self.config.interval_startup_delay_drain_n	 						= vs_config[1]
+		self.config.V_input_max_uV			 						= vs_config[2]
+		self.config.I_input_max_nA			 						= vs_config[3]
+		self.config.V_input_drop_uV			 						= vs_config[4]
+		self.config.R_input_kOhm_n22		 							= vs_config[5]
+		self.config.Constant_us_per_nF_n28		 						= vs_config[6]
+		self.config.V_intermediate_init_uV		 						= vs_config[7]
+		self.config.I_intermediate_leak_nA		 						= vs_config[8]
+		self.config.V_enable_output_threshold_uV	 						= vs_config[9]
+		self.config.V_disable_output_threshold_uV	 						= vs_config[10]
+		self.config.dV_enable_output_uV		 						= vs_config[11]
+		self.config.interval_check_thresholds_n	 						= vs_config[12]
+		self.config.V_pwr_good_enable_threshold_uV 	 						= vs_config[13]
+		self.config.V_pwr_good_disable_threshold_uV	 						= vs_config[14]
+		self.config.immediate_pwr_good_signal	 							= vs_config[15]
+		self.config.V_output_log_gpio_threshold_uV	 						= vs_config[16]
+		self.config.V_input_boost_threshold_uV	 							= vs_config[16]
+		self.config.V_intermediate_max_uV		 						= vs_config[17]
+		self.config.V_output_uV			 						= vs_config[18]
+		self.config.V_buck_drop_uV			 						= vs_config[19]
+		self.config.LUT_input_V_min_log2_uV		 						= vs_config[20]
+		self.config.LUT_input_I_min_log2_nA		 						= vs_config[21]
+		self.config.LUT_output_I_min_log2_nA	 							= vs_config[22]
+		self.config.LUT_inp_efficiency_n8[hvirtual_converter.LUT_SIZE][hvirtual_converter.LUT_SIZE]   = vs_config[23]
+		self.config.LUT_out_inv_efficiency_n4[hvirtual_converter.LUT_SIZE] 				= vs_config[24]		
+		hvirtual_converter.converter_initialize(config)
+		
+	
+	@staticmethod
+	def get_input_efficiency_n8(self, voltage_uV: int, current_nA: int) -> int:
+		voltage_n = int(voltage_uV / (2**self.config.LUT_input_V_min_log2_uV))
+		current_n = int(current_nA / (2**self.config.LUT_input_I_min_log2_nA))
+		pos_v = int(voltage_n) if (voltage_n > 0) else 0  # V-Scale is Linear!
+		pos_c = int(math.log2(current_n)) if (current_n > 0) else 0
+		if pos_v >= hvirtual_converter.LUT_SIZE:
+			pos_v = hvirtual_converter.LUT_SIZE - 1
+		if pos_c >= hvirtual_converter.LUT_SIZE:
+			pos_c = hvirtual_converter.LUT_SIZE - 1
+		return self.config.LUT_inp_efficiency_n8[pos_v * hvirtual_converter.LUT_SIZE + pos_c] / (2**8)
+		
+	@staticmethod
+	def get_output_inv_efficiency_n4(self, current_nA) -> int:
+		current_n = int(current_nA / (2**self.config.LUT_output_I_min_log2_nA))
+		pos_c = int(math.log2(current_n)) if (current_n > 0) else 0
+		if pos_c >= hvirtual_converter.LUT_SIZE:
+			pos_c = hvirtual_converter.LUT_SIZE- 1
+		return self.config.LUT_out_inv_efficiency_n4[pos_c] / (2**4)
 
-def converter_calc_out_power(current_adc_raw):
-	return hvirtual_converter.converter_calc_out_power(current_adc_raw)
+	def converter_calc_inp_power(self, input_voltage_uV: int, input_current_nA: int):
+		return hvirtual_converter.converter_calc_inp_power(input_voltage_uV, input_current_nA)
 
-def get_I_mid_out_nA():
-	return hvirtual_converter.get_I_mid_out_nA()
+	def converter_calc_out_power(self, current_adc_raw):
+		return hvirtual_converter.converter_calc_out_power(current_adc_raw)
 
-def get_V_intermediate_raw():
-	return hvirtual_converter.get_V_intermediate_raw()
+	def get_I_mid_out_nA(self):
+		return hvirtual_converter.get_I_mid_out_nA()
 
-def get_V_intermediate_uV():
-	return hvirtual_converter.get_V_intermediate_uV()
+	def get_V_intermediate_raw(self):
+		return hvirtual_converter.get_V_intermediate_raw()
 
-def get_P_output_fW():
-	return hvirtual_converter.get_P_output_fW()
+	def get_V_intermediate_uV(self):
+		return hvirtual_converter.get_V_intermediate_uV()
 
-def get_P_input_fW():
-	return hvirtual_converter.get_P_input_fW()
+	def get_P_output_fW(self):
+		return hvirtual_converter.get_P_output_fW()
 
-# private fn
-#def py_get_output_inv_efficiency_n4(current_nA):
-#	return get_output_inv_efficiency_n4()
+	def get_P_input_fW(self):
+		return hvirtual_converter.get_P_input_fW()
 
-def set_V_intermediate_uV(C_uV):
-	hvirtual_converter.set_V_intermediate_uV(C_uV)
+	# private fn
+	#def py_get_output_inv_efficiency_n4(current_nA):
+	#	return get_output_inv_efficiency_n4()
+
+	def set_V_intermediate_uV(self, C_uV):
+		hvirtual_converter.set_V_intermediate_uV(C_uV)
+
+	"""	Added from Calibration.c to debug testing.py	"""
+	def cal_conv_adc_raw_to_nA(self, current_raw):
+		return hvirtual_converter.cal_conv_adc_raw_to_nA(current_raw)
+	def cal_conv_uV_to_dac_raw(self, voltage_uV):
+		return hvirtual_converter.cal_conv_uV_to_dac_raw(voltage_uV)
--- software/firmware/pru0-cython-module/testing.py
+++ software/firmware/pru0-cython-module/testing.py
@@ -1,4 +1,51 @@
-import Cvirtual_converter
+import sys
+# sys.path.append("/vishal/shepherd/software/firmware/pru0-cython-module")
+# print(sys.path)
+sys.path.append('/software/firmware/pru0-cython-module')
+# import pyximport
+import numpy as np
 
-output = Cvirtual_converter.py_set_V_intermediate_uV(56)
-print(output)  # noqa: T201
+from virtual_converter import VirtualConverter
+
+#from shepherd.software.virtual_source_config import VirtualSourceConfig
+
+#vscfg = VirtualSourceConfig()
+#vconv = VirtualConverter(vscfg.export_for_sysfs())
+
+#x = int(0)
+#print(VirtualConverter.get_I_mid_out_nA())
+#print(VirtualConverter.converter_calc_inp_power(5, 6))
+#print(VirtualConverter.converter_initialize(0))
+
+class Test():
+	def __init__(self, access):
+		self.access = access
+		
+	def Test_Method1(self):
+		return self.access.get_V_intermediate_uV()
+		
+	def Test_Method2(self):
+		return self.access.get_P_input_fW() 
+		
+	def Test_Method3(self):
+		return self.access.get_P_output_fW() 
+		
+	def Test_Method4(self):
+		return self.access.get_I_mid_out_nA() 
+	
+	def Test_Method5(self):
+		return self.access.get_input_efficiency_n8() 
+		
+if __name__ == "__main__":    
+	TempList = list(np.random.randint(low = 0, high=9, size=25))
+
+	accessVC = VirtualConverter(TempList)
+	accessInstanceTest=Test(accessVC)
+
+	print(accessInstanceTest.Test_Method1())
+	print(accessInstanceTest.Test_Method2())
+	print(accessInstanceTest.Test_Method3())
+	print(accessInstanceTest.Test_Method4())
+	print(accessInstanceTest.Test_Method5())
+    
+    
