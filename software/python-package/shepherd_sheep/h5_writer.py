b3e1eca240fc899943a9acea6d0d7e3db22c3790
Ingmar Splitt <orgua@users.noreply.github.com>
2024-04-27 18:23:22 +0200

    Update h5_writer.py

--- software/python-package/shepherd_sheep/h5_writer.py
+++ software/python-package/shepherd_sheep/h5_writer.py
@@ -296,13 +296,14 @@ class Writer(CoreWriter):
             buf_len = self.grp_data["meta"][buf_iter, 1]
             if buf_len == 0:
                 continue
+            data_pos_end = int(data_pos + buf_len)  # somehow needed
             buf_ts_ns = self.grp_data["meta"][buf_iter, 0]
-            self.grp_data["time"][data_pos : data_pos + buf_len] = (
+            self.grp_data["time"][data_pos : data_pos_end] = (
                 self.buffer_timeseries + buf_ts_ns
             )
             # TODO: not clean - buf_len is read fresh (dynamic), but self.buf_timeseries is static
             # BUT buf_len is either 0 or the static value
-            data_pos += buf_len
+            data_pos = data_pos_end
 
     def start_monitors(
         self,
