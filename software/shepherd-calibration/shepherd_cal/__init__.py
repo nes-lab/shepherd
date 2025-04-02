861460888274b3afe642e5438d7b515cfb2782f4
Ingmar Splitt <orgua@users.noreply.github.com>
2025-04-02 14:37:56 +0200

    Update __init__.py

--- software/shepherd-calibration/shepherd_cal/__init__.py
+++ software/shepherd-calibration/shepherd_cal/__init__.py
@@ -2,8 +2,8 @@ from .calibration_plot import plot_calibration
 from .calibrator import Calibrator
 from .logger import activate_verbosity
 from .logger import logger
-from .profile_cape import ProfileCape
 from .profile_analyzer import analyze_directory
+from .profile_cape import ProfileCape
 from .profiler import Profiler
 
 __version__ = "0.9.0"
