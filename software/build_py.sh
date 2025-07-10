63ad60f7f011177668a009e8e73019dbe1eda158
Ingmar Splitt <orgua@users.noreply.github.com>
2025-07-10 16:21:44 +0200

    improve build-scripts

--- software/build_py.sh
+++ software/build_py.sh
@@ -1,3 +1,4 @@
 # dev-script for faster rebuilding (compared to ansible)
 uv tool install /opt/shepherd/software/python-package/.
+uv cache prune
 # sudo shepherd-sheep -v run /etc/shepherd/example_config_emulation.yaml
