06c9687a166caa81f96e6867c0e42e6a31fd0ed6
Ingmar Splitt <orgua@users.noreply.github.com>
2025-08-21 15:45:46 +0200

    Update h5_recorder_pru.py

--- software/python-package/shepherd_sheep/h5_recorder_pru.py
+++ software/python-package/shepherd_sheep/h5_recorder_pru.py
@@ -30,7 +30,6 @@ class PruRecorder(Monitor):
             "pru0_vsrc_tsample_mean [ns], "
             "pru0_vsrc_tsample_max [ns], "
             f"pru1_gpio_tsample_max [ns/{commons.SAMPLE_INTERVAL_NS}ns]"
-            # f"with {commons.SAMPLE_INTERVAL_NS} ns per sample-step"
         )
         # reset increment AFTER creating all dsets are created
         self.increment = 1000  # 100 s
