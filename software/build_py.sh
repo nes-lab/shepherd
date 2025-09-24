336b01d7338005352e5aa15b8bd0c555fc376863
Ingmar Splitt <orgua@users.noreply.github.com>
2025-09-24 20:41:01 +0200

    Update build_py.sh

--- software/build_py.sh
+++ software/build_py.sh
@@ -1,4 +1,6 @@
 # dev-script for faster rebuilding (compared to ansible)
+sudo systemctl stop shepherd-watchdog
 uv tool install /opt/shepherd/software/python-package/.
 uv cache prune
+sudo systemctl start shepherd-watchdog
 # sudo shepherd-sheep -v run /etc/shepherd/example_config_emulation.yaml
