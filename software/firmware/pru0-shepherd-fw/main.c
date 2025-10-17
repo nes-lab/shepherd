a8db31ca3ac38cf0e6c87d6259035aebb7b733fd
Ingmar Splitt <orgua@users.noreply.github.com>
2025-10-17 21:12:50 +0200

    pru - fix init of timer-var

--- software/firmware/pru0-shepherd-fw/main.c
+++ software/firmware/pru0-shepherd-fw/main.c
@@ -358,7 +358,7 @@ int main(void)
 
 reset:
     msgsys_send(MSG_STATUS_RESTARTING_ROUTINE, 0u, SHARED_MEM.shp_pru0_mode);
-    SHARED_MEM.pru0_ns_per_sample = 0u;
+    SHARED_MEM.pru0_ns_per_sample = IDX_OUT_OF_BOUND;
 
     SHARED_MEM.buffer_iv_inp_idx  = 0u;
     SHARED_MEM.buffer_iv_out_idx  = 0u;
