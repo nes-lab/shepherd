55376fe8ee9c858eea4fedeab8ba2c43fd79ac03
Ingmar Splitt <orgua@users.noreply.github.com>
2025-11-20 10:39:14 +0100

    improve installation via uv / remove venv

--- software/build_py.sh
+++ software/build_py.sh
@@ -1,13 +1,11 @@
 # dev-script for faster rebuilding (compared to ansible)
 sudo systemctl stop shepherd-watchdog
-sudo -E /usr/local/bin/uv tool install /opt/shepherd/software/python-package/. --upgrade --force
-# alternative: sudo -E /usr/local/bin/uv tool upgrade --all
+sudo -E /usr/local/bin/uv pip install /opt/shepherd/software/python-package/.[test] --upgrade --system --break-system-packages
+# will install in /usr/local/bin/shepherd-sheep, check via 'whereis shepherd-sheep'
 sudo -E /usr/local/bin/uv cache prune
 sudo systemctl start shepherd-watchdog
 # remove legacy cache-directories (might be used via ansible)
 sudo rm -rf /home/jane/.cache/uv
 sudo rm -rf /root/.cache/uv
-# try via:
+# check if working via:
 # sudo shepherd-sheep -v run /etc/shepherd/example_config_emulation.yaml
-# for unittests install:
-# sudo /usr/local/bin/uv pip install /opt/shepherd/software/python-package/. --upgrade
