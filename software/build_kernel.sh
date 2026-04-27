1064d53ec513211f08b0b2593588f2267b6ec41e
Ingmar Splitt <orgua@users.noreply.github.com>
2026-04-27 15:00:09 +0200

    make shell scripts less prone to errors

--- software/build_kernel.sh
+++ software/build_kernel.sh
@@ -1,6 +1,6 @@
 # dev-script for faster rebuilding (compared to ansible)
-sudo modprobe -rf shepherd
-sudo modprobe -rf shepherd
+sudo modprobe -rf 'shepherd'
+sudo modprobe -rf 'shepherd'
 #
 cd /opt/shepherd/software/kernel-module/src/
 #
@@ -10,4 +10,4 @@ sudo make install
 #
 cd /opt/shepherd/software/
 #
-sudo modprobe -a shepherd
+sudo modprobe -a 'shepherd'
--- software/build_pru.sh
+++ software/build_pru.sh
@@ -4,26 +4,26 @@ cd /opt/shepherd/software/firmware/pru0-shepherd-fw/
 # EMU
 sudo make clean
 make TYPE=EMU CAPE_HW_VER=$CAPE_HW_VER
-sudo make install TYPE=EMU CAPE_HW_VER=$CAPE_HW_VER
+sudo -E make install TYPE=EMU CAPE_HW_VER=$CAPE_HW_VER
 # HRV
 sudo make clean
 make TYPE=HRV CAPE_HW_VER=$CAPE_HW_VER
-sudo make install TYPE=HRV CAPE_HW_VER=$CAPE_HW_VER
+sudo -E make install TYPE=HRV CAPE_HW_VER=$CAPE_HW_VER
 #
 cd /opt/shepherd/software/firmware/pru1-shepherd-fw/
 # GPIO
 sudo make clean
 make CAPE_HW_VER=$CAPE_HW_VER
-sudo make install CAPE_HW_VER=$CAPE_HW_VER
+sudo -E make install CAPE_HW_VER=$CAPE_HW_VER
 #
 cd /opt/shepherd/software/firmware/pru0-programmer/
 # PRG SWD
 sudo make clean
 make TYPE=SWD CAPE_HW_VER=$CAPE_HW_VER
-sudo make install TYPE=SWD CAPE_HW_VER=$CAPE_HW_VER
+sudo -E make install TYPE=SWD CAPE_HW_VER=$CAPE_HW_VER
 # PROG SBW
 sudo make clean
 make TYPE=SBW CAPE_HW_VER=$CAPE_HW_VER
-sudo make install TYPE=SBW CAPE_HW_VER=$CAPE_HW_VER
+sudo -E make install TYPE=SBW CAPE_HW_VER=$CAPE_HW_VER
 #
 cd /opt/shepherd/software/
