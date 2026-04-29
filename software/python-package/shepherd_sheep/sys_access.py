8651038ada781c19642c0b6ac09e6709512e5c52
Ingmar Splitt <orgua@users.noreply.github.com>
2026-04-29 15:55:58 +0200

    sheep - improve handling of kernel-module

--- software/python-package/shepherd_sheep/sys_access.py
+++ software/python-package/shepherd_sheep/sys_access.py
@@ -5,42 +5,17 @@ from pathlib import Path
 from .logger import log
 
 
-def load_kernel_module() -> None:
-    retry_max: int = 10
-    run_: int = 0
-    path_check = Path("/sys/shepherd/mode")
-    wait_max = 3.0
-    while run_ < retry_max:
-        run_ += 1
-        try:
-            subprocess.run(
-                ["/usr/sbin/modprobe", "--quiet", "shepherd"],
-                timeout=10,
-                check=True,
-                shell=False,
-                capture_output=False,
-            )
-        except subprocess.CalledProcessError:
-            pass
-        else:
-            time_start = time.time()
-            while not path_check.exists():
-                time.sleep(0.1)
-                if time.time() - time_start > wait_max:
-                    break
-            if time.time() - time_start < wait_max:
-                log.debug(
-                    "Activated shepherd kernel module (%d. try, %.1f s wait)",
-                    run_,
-                    time.time() - time_start,
-                )
-                return
-        # cleanup
-        remove_kernel_module()
-        time.sleep(0.2)
-
-    msg = "Failed to load shepherd kernel module."
-    raise SystemError(msg)
+def kernel_module_is_active() -> bool:
+    """Return True if access the sysfs of the kMod."""
+    try:  # test for correct usage -> fail early!
+        # this is sysfs_interface.get_mode() to avoid import
+        with Path("/sys/shepherd/mode").open(encoding="utf-8") as f:
+            _ = f.read()
+    except FileNotFoundError:
+        return False
+    except PermissionError:
+        raise RuntimeError("Failed to access sysFS -> run shepherd-sheep with 'sudo'!") from None
+    return True
 
 
 def remove_kernel_module() -> None:
@@ -72,9 +47,67 @@ def remove_kernel_module() -> None:
     raise SystemError(msg)
 
 
+def load_kernel_module() -> None:
+    retry_max: int = 10
+    run_: int = 0
+    wait_max = 3.0
+    while run_ < retry_max:
+        run_ += 1
+        try:
+            subprocess.run(
+                ["/usr/sbin/modprobe", "--quiet", "shepherd"],
+                timeout=10,
+                check=True,
+                shell=False,
+                capture_output=False,
+            )
+        except subprocess.CalledProcessError:
+            pass
+        else:
+            time_start = time.time()
+            while not kernel_module_is_active():
+                time.sleep(0.1)
+                if time.time() - time_start > wait_max:
+                    break
+            if time.time() - time_start < wait_max:
+                log.debug(
+                    "Activated shepherd kernel module (%d. try, %.1f s wait)",
+                    run_,
+                    time.time() - time_start,
+                )
+                return
+        # cleanup
+        log.warning(
+            "Failed to load shepherd kernel module - will remove & try again (%d. try)", run_
+        )
+        remove_kernel_module()
+        time.sleep(0.2)
+
+    msg = "Failed to load shepherd kernel module."
+    raise SystemError(msg)
+
+
 def reload_kernel_module() -> None:
+    """Makes sure to create a clean slate.
+
+    This will unload, load, test access and raises if
+    - permissions are missing
+    - reload of kMod failed
+    """
+    kernel_module_is_active()  # check for permissions - raise early
     remove_kernel_module()
     load_kernel_module()
+
+
+def check_on_kernel_module() -> None:
+    """Make sure the module is running - start if needed.
+
+    This will test access and unload/load if needed. It raises if
+    - permissions are missing
+    - reload of kMod failed
+    """
+    if not kernel_module_is_active():
+        reload_kernel_module()
 
 
 def disable_ntp() -> None:
@@ -103,41 +136,5 @@ def resync_ptp() -> bool:
     had_error = False
     for command in commands:
         had_error |= subprocess.run(command, timeout=10, check=False).returncode > 0  # noqa: S603
-    reload_kernel_module()
+    reload_kernel_module()  # for restarting sync-loop to PRU
     return had_error
-
-
-def check_sys_access(iteration: int = 1, *, force_kmod_reload: bool = False) -> bool:
-    """Return True if access failed."""
-    retry_max: int = 5
-    if force_kmod_reload:
-        reload_kernel_module()
-    try:  # test for correct usage -> fail early!
-        # this is sysfs_interface.get_mode() to avoid import
-        with Path("/sys/shepherd/mode").open(encoding="utf-8") as f:
-            _ = f.read()
-    except FileNotFoundError:
-        try:
-            if iteration > retry_max:
-                log.error("Failed to access sysFS - ran out of retries")
-                return True
-            log.debug(
-                "Failed to access sysFS -> "
-                "will try to activate shepherd kernel module (attempt %d/%d)",
-                iteration,
-                retry_max,
-            )
-            reload_kernel_module()
-            check_sys_access(iteration + 1)
-        except FileNotFoundError:
-            log.error(
-                "RuntimeError: Failed to access sysFS -> "
-                "make sure shepherd kernel module is active!",
-            )
-            return True
-    except PermissionError:
-        log.error(
-            "RuntimeError: Failed to access sysFS -> run shepherd-sheep with 'sudo'!",
-        )
-        return True
-    return False
